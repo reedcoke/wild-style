64fa681c508cba7b617ccc8ccf70854446e8f589
Tara Safavi <tsafavi@umich.edu>
2015-11-08 02:01:30 -0500

    Added main

--- everythingIsAwesome.py
+++ everythingIsAwesome.py
@@ -1,4 +1,10 @@
-everything = "awesome"
-masterBuilders = ["dumbledore", "batman", "abraham lincoln", "cleopatra"]
-for builder in masterBuilders:
-  print builder, "is", everything
+def main():
+  everything = 'awesome'
+  masterBuilders = ['dumbledore', 'batman', 'abraham lincoln', 'cleopatra']
+  for builder in masterBuilders:
+    print builder, 'is', everything
+
+if __name__ == '__main__':
+  main()
+
+
